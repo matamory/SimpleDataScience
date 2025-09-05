2368b9e69e039587a7bcd850d45ef661d7087fbf
Yesenia Beatriz Matamoros <138043212+matamory@users.noreply.github.com>
2025-09-04 22:14:17 -0400

    basic gender classifier using scikit learn library

---
+++ GenderClassifier/genderClassifier.py
@@ -0,0 +1,20 @@
+from sklearn import tree, svm, neural_network
+
+#[height, weight, shoe size]
+X = [[181,80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177,70, 40], [159, 55,37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male']
+
+clf_1 = tree.DecisionTreeClassifier()
+clf_2 = svm.SVC()
+clf_3 = neural_network.MLPClassifier()
+
+clf_1 = clf_1.fit(X,Y)
+clf_2 = clf_2.fit(X,Y)
+clf_3 = clf_3.fit(X,Y)
+
+prediction_1 = clf_1.predict([[190, 70, 43]])
+prediction_2 = clf_2.predict([[190, 70, 43]])
+prediction_3 = clf_3.predict([[190, 70, 43]])
+
+print(prediction_1, prediction_2, prediction_3, sep= "\n")
