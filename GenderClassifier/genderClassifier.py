d04ccec0daa5043a46f1c10b10369eaa4ba856c0
Yesenia Beatriz Matamoros <138043212+matamory@users.noreply.github.com>
2025-09-05 22:56:41 -0400

    tested out additional classifier on data set

--- GenderClassifier/genderClassifier.py
+++ GenderClassifier/genderClassifier.py
@@ -1,4 +1,4 @@
-from sklearn import tree, svm, neural_network
+from sklearn import tree, svm, neural_network, ensemble
 
 #[height, weight, shoe size]
 X = [[181,80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177,70, 40], [159, 55,37], [171, 75, 42], [181, 85, 43]]
@@ -8,13 +8,16 @@ Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'ma
 clf_1 = tree.DecisionTreeClassifier()
 clf_2 = svm.SVC()
 clf_3 = neural_network.MLPClassifier()
+clf_4 = ensemble.AdaBoostClassifier()
 
 clf_1 = clf_1.fit(X,Y)
 clf_2 = clf_2.fit(X,Y)
 clf_3 = clf_3.fit(X,Y)
+clf_4 = clf_4.fit(X,Y)
 
 prediction_1 = clf_1.predict([[190, 70, 43]])
 prediction_2 = clf_2.predict([[190, 70, 43]])
 prediction_3 = clf_3.predict([[190, 70, 43]])
+prediction_4 = clf_4.predict([[190, 70, 43]])
 
-print(prediction_1, prediction_2, prediction_3, sep= "\n")
+print(prediction_1, prediction_2, prediction_3, prediction_4, sep= "\n")
