0a709931eb81c2b28b06afbad5715e812942eee3
Yesenia Beatriz Matamoros <138043212+matamory@users.noreply.github.com>
2025-09-09 23:28:21 -0400

    Comments added for improved clarity

--- TwitterSentimentAnalyzer/twitterSentimentAnalyzer.py
+++ TwitterSentimentAnalyzer/twitterSentimentAnalyzer.py
@@ -2,16 +2,20 @@ import tweepy
 from textblob import TextBlob
 import pandas
 
+# API credentials
 bearer_token = ***
 
+# Initialize tweepy
 client = tweepy.Client(bearer_token)
 
+# Function to collect recent tweets mentioning string passed in for search query
 def fetch_tweets(query, max_results = 20):
     tweets = client.search_recent_tweets(query=query, max_results=max_results, tweet_field= ['text'])
     return [tweet.text for tweet in tweets.data]
 
 msft_tweets = fetch_tweets("MSFT", max_results=20)
 
+# Function for sentiment analysis using TextBlob and organization of results using DataFram
 def analyze_sentiment(tweets):
     sentiments = [TextBlob(tweet).sentiment.polarity for tweet in tweets]
     return pandas.DataFrame({'Tweet': tweets, 'Sentiment': sentiments})
@@ -20,7 +24,7 @@ msft_sentiment = analyze_sentiment(msft_tweets)
 
 print(msft_sentiments.head())
 
-
+# Determine average sentiment 
 def summarize_sentiments(sentiments_df):
     avg_sentiment = sentiments_df['Sentiment'].mean()
     return avg_sentiment
