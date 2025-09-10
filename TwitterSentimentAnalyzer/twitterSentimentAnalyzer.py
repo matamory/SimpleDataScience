dffd41190907b7cc72910ec65a2b55a14c79def6
Yesenia Beatriz Matamoros <138043212+matamory@users.noreply.github.com>
2025-09-09 23:11:59 -0400

    practice using tweepy library as well as sentiment analysis using Textblob

--- TwitterSentimentAnalyzer/twitterSentimentAnalyzer.py
+++ TwitterSentimentAnalyzer/twitterSentimentAnalyzer.py
@@ -1,1 +1,21 @@
 import tweepy
+from textblob import TextBlob
+import pandas
+
+bearer_token = ***
+
+client = tweepy.Client(bearer_token)
+
+def fetch_tweets(query, max_results = 20):
+    tweets = client.search_recent_tweets(query=query, max_results=max_results, tweet_field= ['text'])
+    return [tweet.text for tweet in tweets.data]
+
+msft_tweets = fetch_tweets("MSFT", max_results=20)
+
+def analyze_sentiment(tweets):
+    sentiments = [TextBlob(tweet).sentiment.polarity for tweet in tweets]
+    return pandas.DataFrame({'Tweet':tweets, 'Sentiment':Sentiments})
+    
+msft_sentiment = analyze_sentiment(msft_tweets)
+
+print(msft_sentiments.head())
